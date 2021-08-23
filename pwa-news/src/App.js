dc150ef1dcc90587c92b0a91f361115183d0fd62
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-23 17:18:19 -0300

    parte do post

--- pwa-news/src/App.js
+++ pwa-news/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './containers/Home';
+import Post from './containers/Post';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <section>
         <Router>
           <Switch>
+            <Route path="/:subject/:id">
+              <Post />
+            </Route>
             <Route path="/">
               <Home />
             </Route>
