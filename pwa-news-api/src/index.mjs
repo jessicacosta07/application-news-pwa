cc7d8400261c80ba0d29ea93690e23211d83aa17
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-23 12:57:04 -0300

    parte api 2

---
+++ pwa-news-api/src/index.mjs
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import economy from './economy.json';
+import technology from './technology.json';
+import world from './world.json';
+
+const GROUP_NEWS = {
+    economy,
+    technology,
+    world
+}
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.get('/api', function (req, res) {
+    res.json({
+        economy,
+        technology,
+        world
+    })
+})
+
+app.listen(PORT, function(){
+    console.log(`Server running on ${PORT} PORT`)
+})
