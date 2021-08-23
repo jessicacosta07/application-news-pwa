4c576091b758c0ddc709c352e63a27c9efe871bd
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-23 18:07:19 -0300

    parte serviceworker e ajustes

--- pwa-news/src/containers/Home.jsx
+++ pwa-news/src/containers/Home.jsx
@@ -1,12 +1,14 @@
-import React, { memo, useEffect, useState } from 'react';
-import { Row, Col } from 'antd';
+import React, { memo, useEffect, useState } from 'react'
+import { Row, Col } from 'antd'
+import Economy from './components/Economy'
+import Technology from './components/Technology'
+import World from './components/World'
 import Api from '../api'
-import Economy from './components/Economy';
-import Technology from './components/Technology';
 
 function Home() {
-    const [news, setNews] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [news, setNews] = useState([])
+    const [loading, setLoading] = useState(false)
+
     const handleNews = (articles) => {
         setLoading(false)
         setNews({
@@ -15,6 +17,7 @@ function Home() {
             technology: articles[2]?.value.value,
         })
     }
+
     useEffect(() => {
         setLoading(true)
         Promise.allSettled([
@@ -24,31 +27,30 @@ function Home() {
         ])
             .then(handleNews)
     }, [])
+
     if (loading) return <div>Carregando</div>
+
     return (
         <div>
             <Row gutter={[16, 16]}>
                 <Col span={24} md={16}>
                     <h2>World</h2>
+                    <World values={news?.world} />
                 </Col>
-            </Row>
-            <hr />
-            <Row gutter={[16, 16]}>
-                <Col span={24} md={16}>
+                <Col span={24} md={8}>
                     <h2>Economy</h2>
                     <Economy values={news?.economy} />
                 </Col>
             </Row>
             <hr />
             <Row gutter={[16, 16]}>
-                <Col span={24} md={16}>
+                <Col span={24}>
                     <h2>Technology</h2>
                     <Technology values={news?.technology} />
                 </Col>
             </Row>
-
         </div>
-    );
+    )
 }
 
-export default memo(Home);
+export default memo(Home)
--- pwa-news/src/containers/components/World.jsx
+++ pwa-news/src/containers/components/World.jsx
@@ -1,29 +1,34 @@
-import React, { memo } from 'react';
-import PropTypes from 'prop-types';
-import { Row, Col } from 'antd';
-import { createMarkup } from '../../utils';
-import { useHistory } from 'react-router-dom';
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col } from 'antd'
+import { createMarkup } from '../../utils'
+import { useHistory } from 'react-router-dom'
 
 function World({ values }) {
     const history = useHistory()
 
-    const renderImg = ({ image, description }) =>
-        <img src={image.url} alt={description} width='100%' />
+    const renderImg = ({ image, description }) => (
+        <div>
+            <img src={image.url} alt={description} width="100%" />
+        </div>
+    )
 
-    const open = (id) => {
-        history.push(`/World/${id}`)
+    const openPost = (id) => {
+        history.push(`/world/${id}`)
     }
 
     const renderPost = (post, index) => {
         const { title, image, description, id } = post
         const isFirst = index === 0
         const spanValue = isFirst ? 24 : 12
+
         return (
-            <Col span={spanValue} md={16} key={`post - ${index}`}>
-                <article onClick={() => open(id)}>
+            <Col span={spanValue} key={`World-${index}`}>
+                <article onClick={() => openPost(id)}>
                     <p>
                         <strong dangerouslySetInnerHTML={createMarkup(title)} />
                     </p>
+                    <p dangerouslySetInnerHTML={createMarkup(description)} />
                     {isFirst && renderImg({ image, description })}
                 </article>
             </Col>
@@ -34,7 +39,7 @@ function World({ values }) {
         <Row gutter={[16, 16]}>
             {values?.map(renderPost)}
         </Row>
-    );
+    )
 }
 
 World.defaultProps = {
@@ -45,4 +50,5 @@ World.propTypes = {
     values: PropTypes.array.isRequired
 }
 
-export default memo(World);
+
+export default memo(World)
