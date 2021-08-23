19a597a2c61601885bb233758a1a11acb16cfc18
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-23 17:40:39 -0300

    parte action e mudanção da url da api

--- pwa-news/src/api.js
+++ pwa-news/src/api.js
@@ -1,30 +1,30 @@
 const params = {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   }
-  
-  
-  const URL = "http://localhost:3000/"
-  
-  function getNews(subject) {
-    return fetch(`${URL}/${subject}`, params)
-      .then((response) => response.json())
-      .catch((err) => {
-        console.error('Ocorreu um err', err)
-      })
-  }
-  
-  function getNewsById(subject, id) {
-    return fetch(`${URL}/${subject}/${id}`, params)
-      .then((response) => response.json())
-      .catch((err) => {
-        console.error('Ocorreu um err', err)
-      })
-  }
-  
-  export default {
-    getNews,
-    getNewsById
-  }
+}
+
+
+const URL = "https://stormy-brook-79548.herokuapp.com/api"
+
+function getNews(subject) {
+  return fetch(`${URL}/${subject}`, params)
+    .then((response) => response.json())
+    .catch((err) => {
+      console.error('Ocorreu um err', err)
+    })
+}
+
+function getNewsById(subject, id) {
+  return fetch(`${URL}/${subject}/${id}`, params)
+    .then((response) => response.json())
+    .catch((err) => {
+      console.error('Ocorreu um err', err)
+    })
+}
+
+export default {
+  getNews,
+  getNewsById
+}
